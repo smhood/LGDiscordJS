108df958590a73afa06bc27124d7efd79c67dc72
smhood <Hoodster92@gmail.com>
2017-07-11 17:41:32 -0500

    fixed config tokens

--- database/_db.js
+++ database/_db.js
@@ -1,6 +1,10 @@
 var Sequelize = require('sequelize')
 var CONFIG = require('./../config.json');
 
-var db = new Sequelize(CONFIG.database, CONFIG.dbUser, CONFIG.dbPassword);
+var db = new Sequelize(CONFIG.database, CONFIG.dbUser, CONFIG.dbPassword, {
+  host: CONFIG.dbhost,
+  port: CONFIG.dbport,
+  timestamps: false
+});
 
 module.exports = db;
